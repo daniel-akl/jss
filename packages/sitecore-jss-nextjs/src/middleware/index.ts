83c8785fc0d412d328effbfd85dc1b8f328b1517
Daniel AKL <danyakl.da@gmail.com>
2022-06-15 15:14:09 +0300

    Delete packages/sitecore-jss-nextjs directory

--- packages/sitecore-jss-nextjs/src/middleware/index.ts
+++
@@ -1,6 +0,0 @@
-export { EditingDataCache } from './editing-data-cache';
-export { EditingDataMiddleware, EditingDataMiddlewareConfig } from './editing-data-middleware';
-export {
-  EditingRenderMiddleware,
-  EditingRenderMiddlewareConfig,
-} from './editing-render-middleware';
--- packages/sitecore-jss-nextjs/test/setup.js
+++
@@ -1,4 +0,0 @@
-require('ts-node/register/transpile-only');
-require('../src/tests/shim.ts');
-require('../src/tests/jsdom-setup.ts');
-require('../src/tests/enzyme-setup.ts');
