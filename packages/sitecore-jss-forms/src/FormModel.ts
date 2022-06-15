4fafc0cc35e292ebd55fdecf0fc28eac53125414
Daniel AKL <danyakl.da@gmail.com>
2022-06-15 15:14:18 +0300

    Delete packages/sitecore-jss-forms directory

--- packages/sitecore-jss-forms/src/FormModel.ts
+++
@@ -1,7 +0,0 @@
-import { ViewModel } from './ViewModel';
-
-export interface FormModel extends ViewModel {
-  isTrackingEnabled: boolean;
-  title: string;
-  cssClass: string;
-}
--- packages/sitecore-jss-forms/src/HtmlFormField.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Represents the components of a HTML form field
- * Used to pass field schema definitions and hidden field values from Sitecore Forms
- */
-export interface HtmlFormField {
-  name: string;
-  id: string;
-  value: string;
-}
